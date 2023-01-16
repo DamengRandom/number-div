4c7836be6b85f61deb72254d664d47c4495719e9
damengrandom <damonmaozewu@gmail.com>
2023-01-16 21:31:46 +1100

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,455 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useForm } from "react-hook-form";
+
+function convertToGua(number) {
+  switch (number) {
+    case 1:
+      return "sky";
+    case 2:
+      return "swamp";
+    case 3:
+      return "fire";
+    case 4:
+      return "thunder";
+    case 5:
+      return "wind";
+    case 6:
+      return "water";
+    case 7:
+      return "mountain";
+    default:
+      return "land";
+  }
+}
+
+function convertToYao(number) {
+  switch (number) {
+    case 1:
+      return 1;
+    case 2:
+      return 2;
+    case 3:
+      return 3;
+    case 4:
+      return 4;
+    case 5:
+      return 5;
+    default:
+      return 6;
+  }
+}
+
+function allGuas(guaCi) {
+  switch (guaCi) {
+    case "sky-sky":
+      return "乾";
+    case "land-land":
+      return "坤";
+    case "water-thunder":
+      return "屯";
+    case "mountain-water":
+      return "蒙";
+    case "water-sky":
+      return "需";
+    case "sky-water":
+      return "讼";
+    case "land-water":
+      return "师";
+    case "water-land":
+      return "比";
+    case "wind-sky":
+      return "小畜";
+    case "sky-swamp":
+      return "履";
+    case "land-sky":
+      return "泰";
+    case "sky-land":
+      return "否";
+    case "sky-fire":
+      return "同人";
+    case "fire-sky":
+      return "大有";
+    case "land-mountain":
+      return "谦";
+    case "thunder-land":
+      return "豫";
+    case "swamp-thunder":
+      return "随";
+    case "mountian-wind":
+      return "蛊";
+    case "land-swamp":
+      return "临";
+    case "wind-land":
+      return "观";
+    case "fire-thunder":
+      return "噬嗑";
+    case "mountian-fire":
+      return "贲";
+    case "mountian-land":
+      return "剥";
+    case "land-thunder":
+      return "复";
+    case "sky-thunder":
+      return "无妄";
+    case "mountian-sky":
+      return "大畜";
+    case "mountian-thunder":
+      return "颐";
+    case "swamp-wind":
+      return "大过";
+    case "water-water":
+      return "坎";
+    case "fire-fire":
+      return "离";
+    case "swamp-mountain":
+      return "咸";
+    case "thunder-wind":
+      return "恒";
+    case "sky-mountain":
+      return "遁";
+    case "thunder-sky":
+      return "大壮";
+    case "fire-land":
+      return "晋";
+    case "land-fire":
+      return "明夷";
+    case "wind-fire":
+      return "家人";
+    case "fire-swamp":
+      return "睽";
+    case "water-mountain":
+      return "蹇";
+    case "thunder-water":
+      return "解";
+    case "mountain-swamp":
+      return "损";
+    case "wind-thunder":
+      return "益";
+    case "swamp-sky":
+      return "夬";
+    case "sky-wind":
+      return "姤";
+    case "swamp-land":
+      return "萃";
+    case "land-wind":
+      return "升";
+    case "swamp-water":
+      return "困";
+    case "water-wind":
+      return "井";
+    case "swamp-fire":
+      return "革";
+    case "fire-wind":
+      return "鼎";
+    case "thunder-thunder":
+      return "震";
+    case "mountain-mountain":
+      return "艮";
+    case "wind-mountain":
+      return "渐";
+    case "thunder-swamp":
+      return "归妹";
+    case "thunder-fire":
+      return "丰";
+    case "fire-mountain":
+      return "旅";
+    case "wind-wind":
+      return "巽";
+    case "swamp-swamp":
+      return "兑";
+    case "wind-water":
+      return "涣";
+    case "water-swamp":
+      return "节";
+    case "wind-swamp":
+      return "中孚";
+    case "thunder-mountain":
+      return "小过";
+    case "water-fire":
+      return "既济";
+    case "fire-water":
+      return "未济";
+  }
+}
+
+function allYaos(gua) {
+  switch (gua) {
+    case "sky-sky":
+    case "乾":
+      return ["初九", "九二", "九三", "九四", "九五", "上九"];
+    case "land-land":
+    case "坤":
+      return ["初六", "六二", "六三", "六四", "六五", "上六"];
+    case "water-thunder":
+    case "屯":
+      return ["初九", "六二", "六三", "六四", "九五", "上六"];
+    case "mountain-water":
+    case "蒙":
+      return ["初六", "九二", "六三", "六四", "六五", "上九"];
+    case "water-sky":
+    case "需":
+      return ["初九", "九二", "九三", "六四", "九五", "上六"];
+    case "sky-water":
+    case "讼":
+      return ["初六", "九二", "六三", "九四", "九五", "上九"];
+    case "land-water":
+    case "师":
+      return ["初六", "九二", "六三", "六四", "六五", "上六"];
+    case "water-land":
+    case "比":
+      return ["初六", "六二", "六三", "六四", "九五", "上六"];
+    case "wind-sky":
+    case "小畜":
+      return ["初九", "九二", "九三", "六四", "九五", "上九"];
+    case "sky-swamp":
+    case "履":
+      return ["初九", "九二", "六三", "九四", "九五", "上九"];
+    case "land-sky":
+    case "泰":
+      return ["初九", "九二", "九三", "六四", "六五", "上六"];
+    case "sky-land":
+    case "否":
+      return ["初六", "六二", "六三", "九四", "九五", "上九"];
+    case "sky-fire":
+    case "同人":
+      return ["初九", "六二", "九三", "九四", "九五", "上九"];
+    case "fire-sky":
+    case "大有":
+      return ["初九", "九二", "九三", "九四", "六五", "上九"];
+    case "land-mountain":
+    case "谦":
+      return ["初六", "六二", "九三", "六四", "六五", "上六"];
+    case "thunder-land":
+    case "豫":
+      return ["初六", "六二", "六三", "九四", "六五", "上六"];
+    case "swamp-thunder":
+    case "随":
+      return ["初九", "六二", "六三", "九四", "九五", "上六"];
+    case "mountian-wind":
+    case "蛊":
+      return ["初六", "九二", "九三", "六四", "六五", "上九"];
+    case "land-swamp":
+    case "临":
+      return ["初九", "九二", "六三", "六四", "六五", "上六"];
+    case "wind-land":
+    case "观":
+      return ["初六", "六二", "六三", "六四", "九五", "上九"];
+    case "fire-thunder":
+    case "噬嗑":
+      return ["初九", "六二", "六三", "九四", "六五", "上九"];
+    case "mountian-fire":
+    case "贲":
+      return ["初九", "六二", "九三", "六四", "六五", "上九"];
+    case "mountian-land":
+    case "剥":
+      return ["初六", "六二", "六三", "六四", "六五", "上九"];
+    case "land-thunder":
+    case "复":
+      return ["初九", "六二", "六三", "六四", "六五", "上六"];
+    case "sky-thunder":
+    case "无妄":
+      return ["初九", "六二", "六三", "九四", "九五", "上九"];
+    case "mountian-sky":
+    case "大畜":
+      return ["初九", "九二", "九三", "六四", "六五", "上九"];
+    case "mountian-thunder":
+    case "颐":
+      return ["初九", "六二", "六三", "六四", "六五", "上九"];
+    case "swamp-wind":
+    case "大过":
+      return ["初六", "九二", "九三", "九四", "九五", "上六"];
+    case "water-water":
+    case "坎":
+      return ["初六", "九二", "六三", "六四", "九五", "上六"];
+    case "fire-fire":
+    case "离":
+      return ["初九", "六二", "九三", "九四", "六五", "上九"];
+    case "swamp-mountain":
+    case "咸":
+      return ["初六", "六二", "九三", "九四", "九五", "上六"];
+    case "thunder-wind":
+    case "恒":
+      return ["初六", "九二", "九三", "九四", "六五", "上六"];
+    case "sky-mountain":
+    case "遁":
+      return ["初六", "六二", "九三", "九四", "九五", "上九"];
+    case "thunder-sky":
+    case "大壮":
+      return ["初九", "九二", "九三", "九四", "六五", "上六"];
+    case "fire-land":
+    case "晋":
+      return ["初六", "六二", "六三", "九四", "六五", "上九"];
+    case "land-fire":
+    case "明夷":
+      return ["初九", "六二", "九三", "六四", "六五", "上六"];
+    case "wind-fire":
+    case "家人":
+      return ["初九", "六二", "九三", "六四", "九五", "上九"];
+    case "fire-swamp":
+    case "睽":
+      return ["初九", "九二", "六三", "九四", "六五", "上九"];
+    case "water-mountain":
+    case "蹇":
+      return ["初六", "六二", "九三", "六四", "九五", "上六"];
+    case "thunder-water":
+    case "解":
+      return ["初六", "九二", "六三", "九四", "六五", "上六"];
+    case "mountain-swamp":
+    case "损":
+      return ["初九", "九二", "六三", "六四", "六五", "上九"];
+    case "wind-thunder":
+    case "益":
+      return ["初九", "六二", "六三", "六四", "九五", "上九"];
+    case "swamp-sky":
+    case "夬":
+      return ["初九", "九二", "九三", "九四", "九五", "上六"];
+    case "sky-wind":
+    case "姤":
+      return ["初六", "九二", "九三", "九四", "九五", "上九"];
+    case "swamp-land":
+    case "萃":
+      return ["初六", "六二", "六三", "九四", "九五", "上六"];
+    case "land-wind":
+    case "升":
+      return ["初六", "九二", "九三", "六四", "六五", "上六"];
+    case "swamp-water":
+    case "困":
+      return ["初六", "九二", "六三", "九四", "九五", "上六"];
+    case "water-wind":
+    case "井":
+      return ["初六", "九二", "九三", "六四", "九五", "上六"];
+    case "swamp-fire":
+    case "革":
+      return ["初九", "六二", "九三", "九四", "九五", "上六"];
+    case "fire-wind":
+    case "鼎":
+      return ["初六", "九二", "九三", "九四", "六五", "上九"];
+    case "thunder-thunder":
+    case "震":
+      return ["初九", "六二", "六三", "九四", "六五", "上六"];
+    case "mountain-mountain":
+    case "艮":
+      return ["初六", "六二", "九三", "六四", "六五", "上九"];
+    case "wind-mountain":
+    case "渐":
+      return ["初六", "六二", "九三", "六四", "九五", "上九"];
+    case "thunder-swamp":
+    case "归妹":
+      return ["初九", "九二", "六三", "九四", "六五", "上六"];
+    case "thunder-fire":
+    case "丰":
+      return ["初九", "六二", "九三", "九四", "六五", "上六"];
+    case "fire-mountain":
+    case "旅":
+      return ["初六", "六二", "九三", "九四", "六五", "上九"];
+    case "wind-wind":
+    case "巽":
+      return ["初六", "九二", "九三", "六四", "九五", "上九"];
+    case "swamp-swamp":
+    case "兑":
+      return ["初九", "九二", "六三", "九四", "九五", "上六"];
+    case "wind-water":
+    case "涣":
+      return ["初六", "九二", "六三", "六四", "九五", "上九"];
+    case "water-swamp":
+    case "节":
+      return ["初九", "九二", "六三", "六四", "九五", "上六"];
+    case "wind-swamp":
+    case "中孚":
+      return ["初九", "九二", "六三", "六四", "九五", "上九"];
+    case "thunder-mountain":
+    case "小过":
+      return ["初六", "六二", "九三", "九四", "六五", "上六"];
+    case "water-fire":
+    case "既济":
+      return ["初九", "六二", "九三", "六四", "九五", "上六"];
+    case "fire-water":
+    case "未济":
+      return ["初六", "九二", "六三", "九四", "六五", "上九"];
+  }
+}
+
+function answer(guaYao) {
+  switch (guaYao) {
+    case "乾初九":
+      return "潜藏的龙 无法施展";
+    case "乾九二":
+      return "龙正在走出低谷 运用德行寻求机会";
+    case "乾九三":
+      return "龙虽处境艰难 但坚持不懈 不断努力";
+    case "乾九四":
+      return "龙跳进深渊 没有灾难";
+    case "乾九五":
+      return "龙在天空中飞 大有所为";
+    case "乾上九":
+      return "升腾到极限的龙会有灾祸之困";
+    case "坤初六":
+      return "阴冷天气逐渐形成 要遵循规律向前推进";
+    case "坤六二":
+      return "向前 前方大道宽广";
+    case "坤六三":
+      return "没有战绩而有好的结果";
+    case "坤六四":
+      return "谨言慎行 就会避免灾祸";
+    case "坤六五":
+      return "黄色的裙裳 大吉大利";
+    case "坤上六":
+      return "人走到了群困的绝境";
+    case "屯初九":
+      return "虽然徘徊难进 但志行贞正 得人心";
+    case "屯六二":
+      return "女性十年才能孕育 前进艰难 现象反常";
+  }
+}
+
+function request(data) {
+  // calculate the gua & yao
+  const xiaGua = convertToGua(data["xia"] % 8);
+  const shangGua = convertToGua(data["shang"] % 8);
+  const yaoCi = convertToYao(data["yao"] % 6);
+
+  // convert gua & yao to one of 64 varients
+  const gua = allGuas(`${shangGua}-${xiaGua}`);
+  const yao = allYaos(gua)[yaoCi];
+
+  console.log(gua, yao);
+}
 
 export default function Home() {
+  const { register, handleSubmit } = useForm();
+  // Concept: enter 3 3 digits numbers, make api call, get result
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Number Divination</title>
+        <meta
+          name="description"
+          content="Number Divination created by Damengrandom"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <form onSubmit={handleSubmit(request)}>
+          <input
+            {...register("xia", { required: true })}
+            placeholder="Please enter first number (3 digits only)"
+          />
+          <input
+            {...register("shang", { required: true })}
+            placeholder="Please enter second number (3 digits only)"
+          />
+          <input
+            {...register("yao", { required: true })}
+            placeholder="Please enter third number (3 digits only)"
+          />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <input type="submit" name="kan" />
+        </form>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+      <footer>Developed by Damengrandom</footer>
+    </>
+  );
 }
